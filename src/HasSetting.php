10cd49c58103beb8f80de96522ac6cf102fa4647
JalalLinuX <smjjalalzadeh93@gmail.com>
2024-01-30 17:47:19 +0330

    Update HasSetting.php

--- src/HasSetting.php
+++ src/HasSetting.php
@@ -9,7 +9,7 @@ trait HasSetting
         return settings()->group(static::class)->get($key);
     }
 
-    public static function setSetting(string $key, $value)
+    public static function setSetting(string $key, $value = null)
     {
         return settings()->group(static::class)->set($key, $value);
     }
